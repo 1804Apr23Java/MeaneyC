61ceef13e28116488ced38df5c365ec5ab3132c3
Collin Meaney <collinmeaney375@gmail.com>
2018-04-26 09:06:14 -0400

    The calculator project as we worked on it in class

---
+++ InvalidInputType.java
@@ -0,0 +1,38 @@
+package scannerCalc;
+
+public class InvalidInputType extends Exception{
+	
+	public InvalidInputType() {
+		super();
+		System.out.println("Invalid input type");
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputType(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputType(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputType(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputType(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
